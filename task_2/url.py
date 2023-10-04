06ab2db5d5c3cf2f18a2891a422a1e17ed8441eb
Alwoch <salwoch@gmail.com>
2023-10-04 18:13:17 +0300

    add finalised task 1 and 2

---
+++ task_2/url.py
@@ -0,0 +1,46 @@
+import csv
+import requests
+
+def get_urls_from_csv(csv_file):
+    """
+    read the csv file and gets the urls
+
+    Args:
+        - csv_file : csv file containing urls to be read
+
+    Returns:
+        - url: url string
+    """
+    with open(csv_file, "r") as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
+        for row in csv_reader:
+            yield row[0]
+
+
+def get_status_code(url):
+    """
+    makes a request to a url and prints the status code and url
+
+    Args:
+        - url (string): url to which a request is to be made
+
+    Returns:
+        - (STATUS CODE) URL : when a request is successfully made
+        - Error URL : when an a request fails
+    """
+    try:
+        response = requests.get(url)
+        status_code = response.status_code
+        print(f"({status_code}) {url}")
+    except requests.exceptions.RequestException:
+        print(f"(Error) {url}")
+
+def main():
+    """prints the status code and url from a csv file"""
+    csv_file='urls.csv'
+    for url in get_urls_from_csv(csv_file):
+        get_status_code(url)
+
+if __name__ == "__main__":
+    main()
